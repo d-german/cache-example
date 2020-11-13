551c4ba9274133921fb1d3549f88567f29a56a49
Damon German <damon.german@hyland.com>
2020-11-13 12:52:17 -0600

    made some changes

---
+++ cache-example/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace cache_example
+{
+    public class Program
+    {
+        private static readonly IDictionary<int, int> Cache = new Dictionary<int, int>();
+
+        private static int ExpensiveCalculation(int value)
+        {
+            Thread.Sleep(2000);
+            return value * 50;
+        }
+
+        private static int GetValue(int key)
+        {
+            if (!Cache.ContainsKey(key))
+            {
+                Cache[key] = ExpensiveCalculation(key);
+            }
+
+            return Cache[key];
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("....");
+            Console.WriteLine(GetValue(5));
+            Console.WriteLine(GetValue(10));
+            Console.WriteLine(GetValue(5));
+            Console.WriteLine(GetValue(10));
+        }
+    }
+}
